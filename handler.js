f6f6b83e575afd5f61fad031212219715169d192
Martijn Gastkemper <martijn@grrr.nl>
2021-06-16 23:08:42 +0200

    Improve error handling and return a nice sentence

--- handler.js
+++ handler.js
@@ -12,43 +12,54 @@ const fetchDay = (api, personId, date) =>
   );
 
 module.exports.slack = async (event) => {
-  const eventBody = Buffer.from(event.body, 'base64');
+  try {
+    const eventBody = Buffer.from(event.body, 'base64');
 
-  const slackParameters = new URLSearchParams(eventBody.toString('ascii'));
+    const slackParameters = new URLSearchParams(eventBody.toString('ascii'));
 
-  const api = axios.create({
-    baseURL: "https://api.productive.io/api/v2/",
-    headers: {
-      "Content-Type": "application/vnd.api+json",
-      "X-Auth-Token": slackParameters.get('text'),
-      "X-Organization-Id": 1376,
-    },
-  });
-
-  const organizationResponse = await api.get("organization_memberships");
-
-  const personId = extractPersonId(organizationResponse);
-
-  const timeEntriesPerDay = await Promise.all([
-    fetchDay(api, personId, "2021-06-15"),
-    fetchDay(api, personId, "2021-06-16"),
-  ]);
-
-  const hoursPerDay = timeEntriesPerDay.map(sumTimeEntries);
-
-  const average = hoursPerDay.reduce((a, b) => a + b, 0) / hoursPerDay.length;
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        average: average / 60,
+    const api = axios.create({
+      baseURL: "https://api.productive.io/api/v2/",
+      headers: {
+        "Content-Type": "application/vnd.api+json",
+        "X-Auth-Token": slackParameters.get('text'),
+        "X-Organization-Id": 1376,
       },
-      null,
-      2
-    ),
-  };
+    });
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    const organizationResponse = await api.get("organization_memberships");
+
+    const personId = extractPersonId(organizationResponse);
+
+    const days = [
+      "2021-06-15",
+      "2021-06-16",
+    ];
+
+    const timeEntriesPerDay = await Promise.all(days.map((day) => fetchDay(api, personId, day), days));
+
+    const hoursPerDay = timeEntriesPerDay.map(sumTimeEntries);
+
+    const average = hoursPerDay.reduce((a, b) => a + b, 0) / hoursPerDay.length;
+
+    return {
+      statusCode: 200,
+      body: `On average you worked ${(average / 60).toFixed(1)} hours during the last ${days.length} days.`,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      let body = JSON.stringify(error.response.data.error);
+      if (error.response.status === 401) {
+        body = `You're not authorized, probably a missing or wrong Productive api key.`;
+      }
+      return {
+        statusCode: 200,
+        body: body,
+      }
+    }
+    return {
+      statusCode: 500,
+      body: error,
+    };
+  }
+
 };
